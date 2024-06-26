06b379229ac3a31c19437b17a61fc2c881344b29
Amir Kooshky <kooshkya@gmail.com>
2024-06-26 20:57:13 +0330

    fix password creation

--- ihms/accounts/serializers.py
+++ ihms/accounts/serializers.py
@@ -7,6 +7,12 @@ class IHMSUserSerializer(serializers.ModelSerializer):
         model = IHMSUser
         fields = ['national_id', 'birthdate', 'gender', 'password']
 
+    def create(self, validated_data):
+        instance = super().create(validated_data)
+        instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
+
 
 class DoctorSerializer(serializers.ModelSerializer):
     user = IHMSUserSerializer()
